5eacf15058b2dcea69d67c09cd2ca8aaf26972fb
Ubuntu <ubuntu@ip-172-31-18-69.us-west-1.compute.internal>
2018-12-03 08:56:20 +0000

    add files

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com\n");
+  response.end("Hello World - Sample App\n");
 });
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://172.17.0.1:8000/");
